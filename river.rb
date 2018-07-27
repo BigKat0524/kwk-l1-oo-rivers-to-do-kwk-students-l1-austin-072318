7a64b45d47ce849004a12ecac83242252ed4ffb6
BigKat0524 <katrinapartain.kk@gmail.com>
2018-07-27 14:31:13 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,32 @@
-# river.rb
+class River 
+  def initialize
+    @discharge = 100
+  end 
+ def name 
+   @name = name 
+ end 
+ def name=(new_name)
+   @name = new_name_name
+ end 
+ def length
+   @length 
+ end 
+ def length=(new_length)
+   @length = new_length
+ end 
+ def countries 
+   @countries
+ end 
+ def countries=(new_count)
+   @countries = new_count
+ end 
+ def flood
+   @discharge += 30 
+ end 
+ def dry_up
+   @discharge -= 50 
+ end 
+ river1 = River.new 
+ 
+ 
+end
